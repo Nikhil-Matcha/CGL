d2b56c20da67892eb363f52464063b18507680e0
Nikhil-Matcha <nikhil.matcha021@msitprogram.net>
2020-12-23 09:39:26 +0530

    version 1.2

--- CGL_Version(2.1)/src/TestJunit.java
+++ CGL_Version(2.1)/src/TestJunit.java
@@ -1,17 +1,20 @@
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
-public class cellTest {
+public class TestJunit {
 	@Test
 	public void testCellOne() {
 		Cell c1 = new Cell(3, 5, true);
 		System.out.println("======TEST ONE EXECUTED=======");
-	    Assertions.assertEquals("Cell at 3,5 is alive.", c1.toString());
+		
+	    Assertions.assertEquals("The Cell at 3,5 is alive.", c1.toString());
 	}
 	
 	@Test
 	public void testCellTwo() {
-		Cell c2 = new Cell(6, 8, true);
+		Cell c2 = new Cell(6, 8, false);
 		System.out.println("======TEST TWO EXECUTED=======");
-	    Assertions.assertEquals("Cell at 6,8 is dead.", c2.toString());
+		System.out.println(c2.toString());
+	    Assertions.assertEquals("The Cell at 6,8 is dead.", c2.toString());
 	}
 }
